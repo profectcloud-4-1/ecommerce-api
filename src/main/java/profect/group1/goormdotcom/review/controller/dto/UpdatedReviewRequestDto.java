9cb1bae7c90d9986f8dea60387e8e9344b624e2b
babogyeong <jbk021010@gmail.com>
2025-10-29 14:39:47 +0900

    ✨[feature]#12 dto 수정

--- src/main/java/profect/group1/goormdotcom/review/controller/dto/UpdatedReviewRequestDto.java
+++ src/main/java/profect/group1/goormdotcom/review/controller/dto/UpdatedReviewRequestDto.java
@@ -11,7 +11,6 @@ public class UpdatedReviewRequestDto {
     @Min(value = 1, message = "별점은 1점 이상이어야 합니다.")
     @Max(value = 5, message = "별점은 5점 이하여야 합니다.")
     private Integer rating;
-
     @NotBlank(message = "리뷰 내용은 필수입니다.")
     @Size(min = 10, max = 500, message = "리뷰는 10자 이상 500자 이하로 작성해주세요.")
     private String content;
